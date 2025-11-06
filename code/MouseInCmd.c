80d865287cb9fdc5ad8a9a07be9414fde45a1707
codeleaded <codeleaded@gmail.com>
2025-11-06 18:18:37 +0100

    update

--- code/MouseInCmd.c
+++ code/MouseInCmd.c
@@ -1,3 +1,6 @@
+#if defined __linux__
+#error "Linux not supported!"
+#elif defined _WINE || defined _WIN32
 #include <windows.h>
 #include <stdio.h>
 
@@ -50,3 +53,4 @@ int main() {
     SetConsoleMode(hInput, prevMode);
     return 0;
 }
+#endif
--- code/WinMain.c
+++ code/WinMain.c
@@ -1,3 +1,6 @@
+#if defined __linux__
+#error "Linux not supported!"
+#elif defined _WINE || defined _WIN32
 #include <windows.h>
 #include <stdio.h>
 
@@ -5,3 +8,4 @@ int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrev,LPSTR lpCmdLine,int nCmdShow)
     printf("Hello World!!!\n");
     return 0;
 }
+#endif
